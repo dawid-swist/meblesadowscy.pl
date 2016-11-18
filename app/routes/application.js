8e054c3a45498b7ead2f8f9354afb843487cc5c4
dawid.swist <dawid.swist@gmail.com>
2016-11-18 02:33:13 +0100

    https://gitlab.com/dawid.swist/meblesadowscy.plissues/32 added posibility edit footer

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,10 +5,12 @@ export default Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
       menu: this.store.findAll('menuElement'),
+      staticElement: this.store.findAll('staticElement')
     });
   },
 
   setupController: function(controller, model) {
     controller.set('menu', model.menu);
+    controller.set('footer', model.staticElement.findBy('id', 'footer'));
   }
 });
